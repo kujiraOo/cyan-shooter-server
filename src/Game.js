55059983b6c39a51d461516d8375ccd6f21c1164
Arsenii Kurilov <kotbegemotoo@gmail.com>
2017-12-10 22:29:31 +0200

    Movement mechanics, multiple player initialization mechanics

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,53 @@
 const Player = require('./Player')
+const p2 = require('p2')
 
 class Game {
   constructor (io) {
     this.io = io
     this.players = []
+    this.world = new p2.World({
+      gravity: [0, 0]
+    })
+
+    this.bounds = {
+      x: 800,
+      y: 600
+    }
+
+    // this.dt = 0
+    // this.start = new Date()
+
+    this.timeStep = 1 / 60
+
+    setInterval(() => {
+      this.update()
+    }, 1000 * this.timeStep)
 
     io.on('connection', (socket) => {
-      const newPlayer = new Player(socket, 50, 50)
-
-      this.players.push(newPlayer)
-
-      socket.emit('playerInitialized', newPlayer.serialize())
+      this.handleClientConnection(socket)
     })
+  }
+
+  handleClientConnection(socket) {
+    const {players, world} = this
+
+    players.forEach((player) => {
+      socket.emit('enemyInitialized', player.serialize())
+    })
+
+    const newPlayer = new Player(this, socket, 50, 50)
+    world.addBody(newPlayer.body)
+
+    players.push(newPlayer)
+
+    socket.emit('playerInitialized', newPlayer.serialize())
+    socket.broadcast.emit('enemyInitialized', newPlayer.serialize())
+  }
+
+  update() {
+    this.world.step(this.timeStep)
+
+    this.players.forEach(player => {player.update()})
   }
 }
 
--- src/Player.js
+++ src/Player.js
@@ -1,8 +1,17 @@
+const p2 = require('p2')
+
 class Player {
-  constructor (socket, x, y) {
+  constructor (game, socket, x, y) {
+    this.game = game
     this.socket = socket
-    this.x = x
-    this.y = y
+    this.speed = 200
+
+    this.body = new p2.Body({
+      mass: 0,
+      position: [x, y]
+    })
+
+    this.body.addShape(new p2.Circle({ radius: 1 }))
 
     this.input = {
       up: false,
@@ -19,13 +28,79 @@ class Player {
   handlePlayerInput(input) {
     const key = Object.keys(input)[0]
     this.input[key] = input[key]
-    console.log(this.input)
+  }
+
+  update() {
+    this.move()
+  }
+  
+  move() {
+    const {input, body, speed} = this
+
+    body.velocity[0] = 0
+    body.velocity[1] = 0
+
+    if (input.up) {
+      body.velocity[1] = -speed
+    }
+
+    if (input.right) {
+      body.velocity[0] = speed
+    }
+
+    if (input.down) {
+      body.velocity[1] = speed
+    }
+
+    if (input.left) {
+      body.velocity[0] = -speed
+    }
+
+    if (input.down && input.right) {
+      body.velocity[0] = Math.cos(0.25 * Math.PI) * speed
+      body.velocity[1] = Math.sin(0.25 * Math.PI) * speed
+    }
+
+    if (input.down && input.left) {
+      body.velocity[0] = Math.cos(0.75 * Math.PI) * speed
+      body.velocity[1] = Math.sin(0.75 * Math.PI) * speed
+    }
+
+    if (input.up && input.right) {
+      body.velocity[0] = Math.cos(-0.25 * Math.PI) * speed
+      body.velocity[1] = Math.sin(-0.25 * Math.PI) * speed
+    }
+
+    if (input.up && input.left) {
+      body.velocity[0] = Math.cos(-0.75 * Math.PI) * speed
+      body.velocity[1] = Math.sin(-0.75 * Math.PI) * speed
+    }
+
+    if (body.position[0] < 0) {
+      body.position[0] = 0
+    }
+
+    if (body.position[1] < 0) {
+      body.position[1] = 0
+    }
+
+    if (body.position[0] > this.game.bounds.x) {
+      body.position[0] = this.game.bounds.x
+    }
+
+    if (body.position[1] > this.game.bounds.y) {
+      body.position[1] = this.game.bounds.y
+    }
+
+    console.log(body.position[0], body.position[1], body.velocity[0], body.velocity[1])
+
+    this.socket.emit('playerMove', {x: body.position[0], y: body.position[1]})
   }
 
   serialize() {
     return {
-      x: this.x,
-      y: this.y,
+      x: this.body.position[0],
+      y: this.body.position[1],
       id: this.socket.id
     }
   }
