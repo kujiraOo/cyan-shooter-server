b87886e9242713a2f3d89d9c831b36e640a540bc
Arsenii Kurilov <kotbegemotoo@gmail.com>
2017-12-17 02:01:08 +0200

    App now serves static files

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Game = require('./src/Game')
 
-const app = require('express')()
+const express = require('express')
+const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+
+app.use(express.static('public/cyan-shooter'))
 
 new Game(io)
 
