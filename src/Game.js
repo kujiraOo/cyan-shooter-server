fa1c78578f7d8133ce326991d893155133c9d287
Arsenii Kurilov <kotbegemotoo@gmail.com>
2018-03-05 19:55:03 +0200

    Implemented player slot system

--- src/Game.js
+++ src/Game.js
@@ -3,12 +3,19 @@ const p2 = require('p2')
 const { SPAWN_OFFSET } = require('./const')
 const { getRandomInt } = require('./utils')
 
-const MAX_PLAYERS = 6
-
 class Game {
-  constructor (io) {
+  constructor(io) {
     this.io = io
-    this.players = []
+
+    this.players = {
+      PLAYER_1: null,
+      PLAYER_2: null,
+      PLAYER_3: null,
+      PLAYER_4: null,
+      PLAYER_5: null,
+      PLAYER_6: null
+    }
+
     this.world = new p2.World({
       gravity: [0, 0]
     })
@@ -31,72 +38,122 @@ class Game {
       this.handleClientConnection(socket)
     })
 
-    this.world.on('impact', ({ bodyA, bodyB }) => {
-      let player, bullet
-
-      if (bodyA.gameEntityType === 'BULLET') {
-        player = bodyB.player
-        bullet = bodyA.bullet
-
-      } else {
-        player = bodyA.player
-        bullet = bodyB.bullet
-      }
-
-      const wasKilled = player.hit(bullet.damage)
-      if (wasKilled) {
-        bullet.player.increaseKillScore()
-      }
-      bullet.destroy()
+    this.world.on('impact', (bodies) => {
+      this.handleCollision(bodies)
     })
   }
 
   handleClientConnection (socket) {
     const { players, world } = this
 
-    if (players.length < MAX_PLAYERS) {
-      players.forEach((player) => {
-        socket.emit('enemyInitialized', player.serialize())
+    const slotId = this.findFreePlayerSlotId()
+
+    if (slotId) {
+
+      Object.keys(this.players).forEach(slotId => {
+        const player = this.players[slotId]
+
+        if (player) {
+          socket.emit('enemyInitialized', player.serialize())
+        }
       })
 
-      const collisionGroupId = 'PLAYER_' + (players.length + 1)
+      const collisionGroupId = slotId
       const newPlayer = new Player(
-        players.length, this, socket,
+        slotId, this, socket,
         getRandomInt(SPAWN_OFFSET, this.bounds.x - SPAWN_OFFSET),
         getRandomInt(SPAWN_OFFSET, this.bounds.y - SPAWN_OFFSET),
         collisionGroupId
       )
 
       world.addBody(newPlayer.body)
-      players.push(newPlayer)
+      this.players[slotId] = newPlayer
 
       socket.emit('playerInitialized', newPlayer.serialize())
       socket.broadcast.emit('enemyInitialized', newPlayer.serialize())
-      console.log(players.length)
+      console.log('Added player to slot: ' + slotId)
 
       socket.on('disconnect', () => {
         this.handleClientDisconnect(socket)
-        console.log(players.length)
       })
+    } else {
+      console.log('No free player slots')
     }
+  }
+
+  findFreePlayerSlotId () {
+    const slotIds = Object.keys(this.players)
+
+    for (let i = 0; i < slotIds.length; i++) {
+      const slotId = slotIds[i]
+
+      if (this.players[slotId] === null) {
+        return slotId
+      }
+    }
+
+    return null
+  }
+
+  findSlotIdBySocket (socket) {
+    const slotIds = Object.keys(this.players)
+
+    for (let i = 0; i < slotIds.length; i++) {
+      const slotId = slotIds[i]
+      const player = this.players[slotId]
+
+      if (player && player.socket.id === socket.id) {
+        return slotId
+      }
+    }
+
+    return null
   }
 
   handleClientDisconnect (socket) {
     const { players, world } = this
-    const playerIdx = players.findIndex(player => player.socket.id === socket.id)
-    const player = players[playerIdx]
+    const slotId = this.findSlotIdBySocket(socket)
+    const player = players[slotId]
 
-    world.removeBody(player.body)
-    players.splice(playerIdx, 1)
+    if (player) {
+      world.removeBody(player.body)
+      players[slotId] = null
+      socket.broadcast.emit('playerRemoved', socket.id)
 
-    socket.broadcast.emit('playerRemoved', socket.id)
+      console.log("Removed player " + slotId)
+    }
+  }
+
+  handleCollision ({ bodyA, bodyB }) {
+    let player, bullet
+
+    if (bodyA.gameEntityType === 'BULLET') {
+      player = bodyB.player
+      bullet = bodyA.bullet
+
+    } else {
+      player = bodyA.player
+      bullet = bodyB.bullet
+    }
+
+    const wasKilled = player.hit(bullet.damage)
+    if (wasKilled) {
+      bullet.player.increaseKillScore()
+    }
+    bullet.destroy()
   }
 
   update () {
     this.world.step(this.timeStep)
 
-    this.players.forEach(player => {
-      player.update()
+    const playerSlotIds = Object.keys(this.players)
+
+    playerSlotIds.forEach(slotId => {
+      const player = this.players[slotId]
+
+      if (player !== null) {
+        player.update()
+      }
     })
   }
 }
